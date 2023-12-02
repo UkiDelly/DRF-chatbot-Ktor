327a21508da03b1974161b2d2d3960ad29979aec
UkiDelly <daehyeon1999@naver.com>
2023-12-02 21:21:47 +0900

    ✨add: DB 연결

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -2,17 +2,21 @@ package com.example
 
 import com.example.plugins.*
 import io.ktor.server.application.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
-	io.ktor.server.netty.EngineMain.main(args)
-}
+fun main(args: Array<String>) = EngineMain.main(args)
+
 
 fun Application.module() {
+	val url = environment.config.property("database.url").getString()
+	val user = environment.config.property("database.user").getString()
+	val password = environment.config.property("database.password").getString()
+	DatabaseFactory.init(url, user, password)
+	
 	configureSecurity()
 	configureHTTP()
 	configureMonitoring()
 	configureSerialization()
-	configureDatabases()
 	configureSockets()
 	configureRouting()
 }
