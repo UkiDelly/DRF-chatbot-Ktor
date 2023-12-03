6460f4fc959f4fa8ba99d9218d60175aa0d5b985
UkiDelly <daehyeon1999@naver.com>
2023-12-03 17:07:53 +0900

    🏷️ update: User 테이블에 누락된 컬럼 추가

--- src/main/kotlin/com/example/database/entity/UserEntity.kt
+++ src/main/kotlin/com/example/database/entity/UserEntity.kt
@@ -6,18 +6,19 @@ import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 
 class UserEntity(id: EntityID<Int>) : IntEntity(id) {
-	
-	companion object : IntEntityClass<UserEntity>(UserTable)
-	
-	var nickname by UserTable.nickname
-	var email by UserTable.email
-	var socialType by UserTable.socialType
-	var snsId by UserTable.snsId
-	var password by UserTable.password
-	var chatCount by UserTable.chatCount
-	var isActive by UserTable.isActive
-	var isAdmin by UserTable.isAdmin
-	var lastLogin by UserTable.lastLogin
-	var createdAt by UserTable.createdAt
-	
+  
+  companion object : IntEntityClass<UserEntity>(UserTable)
+  
+  var nickname by UserTable.nickname
+  var email by UserTable.email
+  var socialType by UserTable.socialType
+  var snsId by UserTable.snsId
+  var password by UserTable.password
+  var chatCount by UserTable.chatCount
+  var isActive by UserTable.isActive
+  var isAdmin by UserTable.isAdmin
+  var lastLogin by UserTable.lastLogin
+  var createdAt by UserTable.createdAt
+  var updatedAt by UserTable.updatedAt
+  
 }
--- src/main/kotlin/com/example/database/table/UserTable.kt
+++ src/main/kotlin/com/example/database/table/UserTable.kt
@@ -1,19 +1,17 @@
 package com.example.database.table
 
 import com.example.common.SocialType
-import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.javatime.datetime
 import java.time.LocalDateTime
 
-object UserTable : IntIdTable("users") {
-	val nickname = varchar("nickname", length = 100)
-	val email = varchar("email", length = 255)
-	val socialType = enumerationByName<SocialType>("social_type", 10)
-	val snsId = varchar("sns_id", length = 255).nullable()
-	val password = text("password").nullable()
-	val chatCount = integer("chat_count").default(0)
-	val isActive = bool("is_active").default(true)
-	val isAdmin = bool("is_admin").default(false)
-	val lastLogin = datetime("last_login").clientDefault { LocalDateTime.now() }
-	val createdAt = datetime("created_at").clientDefault { LocalDateTime.now() }
+object UserTable : BaseTimeTable("users") {
+  val nickname = varchar("nickname", length = 100)
+  val email = varchar("email", length = 255)
+  val socialType = enumerationByName<SocialType>("social_type", 10)
+  val snsId = varchar("sns_id", length = 255).nullable()
+  val password = text("password").nullable()
+  val chatCount = integer("chat_count").default(0)
+  val isActive = bool("is_active").default(true)
+  val isAdmin = bool("is_admin").default(false)
+  val lastLogin = datetime("last_login").clientDefault { LocalDateTime.now() }
 }
