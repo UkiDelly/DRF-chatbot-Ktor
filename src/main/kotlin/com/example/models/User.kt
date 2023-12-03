66fdc616c514e9f3c13d7dad24935d027f8b1e11
UkiDelly <daehyeon1999@naver.com>
2023-12-03 17:08:17 +0900

    🏷️ update: User에 누락된 속성 추가

--- src/main/kotlin/com/example/models/User.kt
+++ src/main/kotlin/com/example/models/User.kt
@@ -9,31 +9,34 @@ import java.time.LocalDateTime
 
 @Serializable
 data class User(
-	val id: Int,
-	val nickname: String,
-	val email: String,
-	val socialType: SocialType,
-	val snsId: String,
-	val password: String?,
-	val chatCount: Int,
-	val isActive: Boolean,
-	val isAdmin: Boolean,
-	@Serializable(with = LocalDateTimeSerializer::class)
-	val lastLogin: LocalDateTime,
-	@Serializable(with = LocalDateTimeSerializer::class)
-	val createdAt: LocalDateTime
+  val id: Int,
+  val nickname: String,
+  val email: String,
+  val socialType: SocialType,
+  val snsId: String?,
+  val password: String?,
+  val chatCount: Int,
+  val isActive: Boolean,
+  val isAdmin: Boolean,
+  @Serializable(with = LocalDateTimeSerializer::class)
+  val lastLogin: LocalDateTime,
+  @Serializable(with = LocalDateTimeSerializer::class)
+  val createdAt: LocalDateTime,
+  @Serializable(with = LocalDateTimeSerializer::class)
+  val updatedAt: LocalDateTime
 ) {
-	constructor(entity: UserEntity) : this(
-		id = entity.id.value,
-		nickname = entity.nickname,
-		email = entity.email,
-		socialType = entity.socialType,
-		snsId = entity.snsId,
-		password = entity.password,
-		chatCount = entity.chatCount,
-		isActive = entity.isActive,
-		isAdmin = entity.isAdmin,
-		lastLogin = entity.lastLogin,
-		createdAt = entity.createdAt
-	)
+  constructor(entity: UserEntity) : this(
+    id = entity.id.value,
+    nickname = entity.nickname,
+    email = entity.email,
+    socialType = entity.socialType,
+    snsId = entity.snsId,
+    password = entity.password,
+    chatCount = entity.chatCount,
+    isActive = entity.isActive,
+    isAdmin = entity.isAdmin,
+    lastLogin = entity.lastLogin,
+    createdAt = entity.createdAt,
+    updatedAt = entity.updatedAt
+  )
 }
