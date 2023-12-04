1737783acfda930780e8465a480c0d894f87304b
UkiDelly <daehyeon1999@naver.com>
2023-12-04 09:05:53 +0900

    🎨 update: 로그인 로직 수정

--- src/main/kotlin/com/example/plugins/StatusPage.kt
+++ src/main/kotlin/com/example/plugins/StatusPage.kt
@@ -5,6 +5,7 @@ import com.example.common.ConflictException
 import com.example.models.response.ErrorResponse
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.requestvalidation.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
@@ -25,6 +26,11 @@ fun Application.configureStatusPage() {
     // Request Validation 에서
     exception<RequestValidationException> { call, cause ->
       call.respond(HttpStatusCode.BadRequest, ErrorResponse(cause.reasons))
+    }
+    
+    // Not Found 에러
+    exception<NotFoundException> { call, cause ->
+      call.respond(HttpStatusCode.NotFound, ErrorResponse(cause.message))
     }
   }
 }
--- src/main/kotlin/com/example/service/AccountService.kt
+++ src/main/kotlin/com/example/service/AccountService.kt
@@ -1,5 +1,6 @@
 package com.example.service
 
+import at.favre.lib.crypto.bcrypt.BCrypt
 import com.example.common.ConflictException
 import com.example.common.SocialType
 import com.example.database.entity.UserEntity
@@ -10,8 +11,8 @@ import com.example.plugins.DatabaseFactory.query
 import io.ktor.server.plugins.*
 import org.jetbrains.exposed.sql.and
 import org.koin.core.annotation.Single
-import org.mindrot.jbcrypt.BCrypt
 import org.slf4j.LoggerFactory
+import java.time.LocalDateTime
 
 
 @Single
@@ -20,7 +21,7 @@ class AccountService {
   private val logger = LoggerFactory.getLogger(this::class.java)
   
   suspend fun register(request: RegisterRequestDto): User {
-    val hashedPassword = BCrypt.hashpw(request.password, "$2a$10$")
+    val hashedPassword = BCrypt.withDefaults().hashToString(12, request.password !!.toCharArray())
     val newUser = query {
       UserEntity.find { UserTable.email eq request.email }.firstOrNull()
         .also { if (it != null) throw ConflictException("이미 존재하는 유저입니다.") }
@@ -38,21 +39,28 @@ class AccountService {
   suspend fun findSocialUser(snsId: String): User {
     val userEntity = query {
       UserEntity.find { (UserTable.snsId eq snsId) and (UserTable.socialType eq SocialType.google) }.firstOrNull()
-    }
-      ?: throw NotFoundException("유저가 존재하지 않습니다.")
+        ?.also { it.lastLogin = LocalDateTime.now() }
+      
+    } ?: throw NotFoundException("유저가 존재하지 않습니다.")
+    
+    
     return User(userEntity)
   }
   
   suspend fun findEmailUser(email: String, password: String): User {
-    val hashedPassword = BCrypt.hashpw(password, "$2a$10$")
-    val userEntityList = query { UserEntity.find { (UserTable.email eq email) }.toList() }.also {
-      if (it.isEmpty()) throw NotFoundException("유저가 존재하지 않습니다.")
+    val userEntity = query {
+      UserEntity.find { (UserTable.email eq email) and (UserTable.socialType eq SocialType.email) }
+        .find { BCrypt.verifyer().verify(password.toCharArray(), it.password).verified }
+    } ?: throw NotFoundException("유저가 존재하지 않습니다.")
+    
+    return User(userEntity)
+  }
+  
+  
+  suspend fun findUserById(id: Int): User {
+    val userEntity = query {
+      UserEntity.findById(id) ?: throw NotFoundException("유저가 존재하지 않습니다.")
     }
-    for (userEntity in userEntityList) {
-      if (userEntity.password == hashedPassword) {
-        return User(userEntity)
-      }
-    }
-    throw NotFoundException("비밀번호가 맞지 않습니다.")
+    return User(userEntity)
   }
 }
