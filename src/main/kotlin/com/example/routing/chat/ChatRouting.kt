8ef49391333ce3c413a355b6ca2cc3a15e0be929
UkiDelly <daehyeon1999@naver.com>
2023-12-04 10:46:32 +0900

    ✨ add: 채팅방 리스트 API 개발

--- src/main/kotlin/com/example/routing/chat/ChatRouting.kt
+++ src/main/kotlin/com/example/routing/chat/ChatRouting.kt
@@ -1,15 +1,24 @@
 package com.example.routing.chat
 
+import com.example.models.response.SuccessResponse
 import com.example.plugins.getUserId
+import com.example.service.ChatService
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.resources.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.Route
+import org.koin.ktor.ext.inject
 
 
 fun Route.chatRouting() {
   
+  val chatService by inject<ChatService>()
+  
   get<ChatRoutes> {
     val userId = call.getUserId()
+    val chatRooms = chatService.listChatRoom(userId)
+    call.respond(HttpStatusCode.OK, SuccessResponse(chatRooms))
   }
   
   post<ChatRoutes> {
