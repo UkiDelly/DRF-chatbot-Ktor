3cea721d64d31a58f8cac2985eeb69f5166f8594
UkiDelly <daehyeon1999@naver.com>
2023-12-04 09:39:40 +0900

    🏷 add: ChatService 생성 및 등록

--- src/main/kotlin/com/example/plugins/Koin.kt
+++ src/main/kotlin/com/example/plugins/Koin.kt
@@ -1,13 +1,17 @@
 package com.example.plugins
 
 import com.example.service.AccountService
+import com.example.service.ChatService
 import io.ktor.server.application.*
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 
 fun Application.configureKoin() {
   install(Koin) {
-    val services = module { single { AccountService() } }
+    val services = module {
+      single { AccountService() }
+      single { ChatService() }
+    }
     modules(services)
   }
 }
---
+++ src/main/kotlin/com/example/service/ChatService.kt
@@ -0,0 +1,7 @@
+package com.example.service
+
+import org.koin.core.annotation.Module
+
+
+@Module
+class ChatService 
