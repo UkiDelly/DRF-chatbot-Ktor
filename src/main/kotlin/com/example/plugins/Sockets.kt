83cd9aa3c9a88b3eb69eefae12872bf4b157ae6f
UkiDelly <daehyeon1999@naver.com>
2023-12-07 19:26:49 +0900

    💡 comment: 웹소켓 로직에 설명 주석 추가

--- src/main/kotlin/com/example/plugins/Sockets.kt
+++ src/main/kotlin/com/example/plugins/Sockets.kt
@@ -10,36 +10,43 @@ import org.koin.ktor.ext.inject
 import java.time.Duration
 
 fun Application.configureSockets() {
-  install(WebSockets) {
-    pingPeriod = Duration.ofSeconds(15)
-    timeout = Duration.ofSeconds(15)
-    maxFrameSize = Long.MAX_VALUE
-    masking = false
-  }
-  routing {
-    val chatService by inject<ChatService>()
-    
-    webSocket("/chat/{chatRoomId}") {
-      val chatRoomId = call.parameters["chatRoomId"]?.toInt()
-      
-      
-      val openAiToken = application.environment.config.property("openai.token").getString()
-      val openAiClient = OpenAiClient(openAiToken)
-      
-      if (chatRoomId == null) {
-        close(CloseReason(CloseReason.Codes.NORMAL, "ChatRoomId is null."))
-        return@webSocket
-      }
-      
-      for (frame in incoming) {
-        if (frame is Frame.Text) {
-          val text = frame.readText()
-          // chatService.socket(chatRoomId, text)
-          val messages = chatService.getAllMessages(chatRoomId)
-          val res = openAiClient.sendChat(messages)
-          outgoing.send(Frame.Text("하이"))
-        }
-      }
-    }
-  }
+	install(WebSockets) {
+		pingPeriod = Duration.ofSeconds(15)
+		timeout = Duration.ofSeconds(15)
+		maxFrameSize = Long.MAX_VALUE
+		masking = false
+	}
+	routing {
+		val chatService by inject<ChatService>()
+		
+		webSocket("/chat/{chatRoomId}") {
+			val chatRoomId = call.parameters["chatRoomId"]?.toInt()
+			
+			
+			val openAiToken = application.environment.config.property("openai.token").getString()
+			val openAiClient = OpenAiClient(openAiToken)
+			
+			if (chatRoomId == null) {
+				close(CloseReason(CloseReason.Codes.NORMAL, "ChatRoomId is null."))
+				return@webSocket
+			}
+			
+			for (frame in incoming) {
+				if (frame is Frame.Text) {
+					val text = frame.readText()
+					// DB에 데이터 추가
+					// chatService.socket(chatRoomId, text)
+					
+					// DB에서 채팅방의 채팅 내역 조회
+					val messages = chatService.getAllMessages(chatRoomId)
+					// 챗지피티 대답
+					val res = openAiClient.sendChat(messages)
+					// DB에 대답 저장
+					chatService.addAssistantMessage(chatRoomId, res)
+					// 웹소켓에 대답 보내기
+					outgoing.send(Frame.Text("하이"))
+				}
+			}
+		}
+	}
 }
--- src/main/kotlin/com/example/service/ChatService.kt
+++ src/main/kotlin/com/example/service/ChatService.kt
@@ -22,134 +22,140 @@ import org.koin.core.annotation.Module
 
 @Module
 class ChatService {
-  
-  suspend fun listChatRoom(userId: Int): List<ChatRoom> {
-    return query {
-      val chatRoomsEntity = ChatRoomEntity.find { ChatRoomTable.userId eq userId }.toList()
-      chatRoomsEntity.map { ChatRoom(it) }
-    }
-    
-  }
-  
-  suspend fun createChatRoom(userId: Int, reqeust: ChatRoomCreateReqeustDto): ChatRoomDetail {
-    val chatRoom = query {
-      val user = UserEntity.findById(userId) ?: throw NotFoundException("유저가 존재하지 않습니다.")
-      ChatRoomDetailEntity.new {
-        name = reqeust.name
-        this.user = user
-      }
-    }
-    
-    return query {
-      SystemPromptEntity.new {
-        this.chatRoom = chatRoom
-        this.prompt = reqeust.systemPrompt
-      }
-      ChatRoomDetail(chatRoom)
-    }
-  }
-  
-  suspend fun getChatRoom(chatRoomId: Int): ChatRoomDetail {
-    return query {
-      val chatRoom = findChatRoom(chatRoomId)
-      ChatRoomDetail(chatRoom)
-    }
-  }
-  
-  
-  suspend fun updateChatRoom(chatRoomId: Int, name: String) {
-    query {
-      val chatRoom = findChatRoom(chatRoomId)
-      chatRoom.name = name
-    }
-  }
-  
-  suspend fun deleteChatRoom(chatRoomId: Int) {
-    query {
-      val chatRoom = findChatRoom(chatRoomId)
-      chatRoom.delete()
-    }
-  }
-  
-  
-  suspend fun addChatHistory(chatRoomId: Int, message: String, role: Role): ChatHistory {
-    val chatHistory = query {
-      val chatRoom = findChatRoom(chatRoomId)
-      ChatHistoryEntity.new {
-        this.chatRoom = chatRoom
-        this.message = message
-        this.role = role
-      }
-    }
-    return ChatHistory(chatHistory)
-  }
-  
-  suspend fun listChatHitory(chatRoomId: Int): List<ChatHistory> {
-    return query {
-      val chatHistories = ChatHistoryEntity.find { ChatHistoryTable.chatRoomId eq chatRoomId }.toList()
-      chatHistories.map { ChatHistory(it) }
-    }
-  }
-  
-  suspend fun addSystemPrompt(chatRoomId: Int, prompt: String) {
-    query {
-      val chatRoom = findChatRoom(chatRoomId)
-      SystemPromptEntity.new {
-        this.chatRoom = chatRoom
-        this.prompt = prompt
-      }
-    }
-  }
-  
-  
-  suspend fun updateSystemPrompt(chatRoomId: Int, prompt: String) {
-    query {
-      val systemPrompt =
-        SystemPromptEntity.find { SystemPromptTable.chatRoomId eq chatRoomId }.firstOrNull() ?: throw NotFoundException(
-          "채팅방이 존재하지 않습니다."
-        )
-      systemPrompt.prompt = prompt
-    }
-  }
-  
-  
-  suspend fun deleteSystemPrompt(chatRoomId: Int) {
-    query {
-      val systemPrompt =
-        SystemPromptEntity.find { SystemPromptTable.chatRoomId eq chatRoomId }.firstOrNull() ?: throw NotFoundException(
-          "채팅방이 존재하지 않습니다."
-        )
-      systemPrompt.delete()
-    }
-  }
-  
-  suspend fun getAllMessages(chatRoomId: Int): List<BaseModel> {
-    return query {
-      val systemPrompt =
-        SystemPromptEntity.find { SystemPromptTable.chatRoomId eq chatRoomId }.map { SystemPrompt(it) }
-      
-      val chatRoom = findChatRoom(chatRoomId)
-      val chatHistories = ChatHistoryEntity.find { ChatHistoryTable.chatRoomId eq chatRoomId }.toList()
-      val messages = systemPrompt.map { SystemModel(it.prompt) } + chatHistories.map {
-        if (it.role == Role.user) UserModel(it.message)
-        else AssistantModel(it.message)
-      }
-      messages
-    }
-  }
-  
-  
-  suspend fun socket(chatRoomId: Int, message: String) {
-    return query {
-      val user = ChatRoomEntity.findById(chatRoomId)?.user ?: throw NotFoundException("채팅방이 존재하지 않습니다.")
-      if (user.chatCount >= 5) throw ReachedLimitException()
-      user.chatCount ++
-      addChatHistory(chatRoomId, message, Role.user)
-    }
-  }
-  
-  
-  private fun findChatRoom(chatRoomId: Int): ChatRoomDetailEntity {
-    return ChatRoomDetailEntity.findById(chatRoomId) ?: throw NotFoundException("채팅방이 존재하지 않습니다.")
-  }
+	
+	suspend fun listChatRoom(userId: Int): List<ChatRoom> {
+		return query {
+			val chatRoomsEntity = ChatRoomEntity.find { ChatRoomTable.userId eq userId }.toList()
+			chatRoomsEntity.map { ChatRoom(it) }
+		}
+		
+	}
+	
+	suspend fun createChatRoom(userId: Int, reqeust: ChatRoomCreateReqeustDto): ChatRoomDetail {
+		val chatRoom = query {
+			val user = UserEntity.findById(userId) ?: throw NotFoundException("유저가 존재하지 않습니다.")
+			ChatRoomDetailEntity.new {
+				name = reqeust.name
+				this.user = user
+			}
+		}
+		
+		return query {
+			SystemPromptEntity.new {
+				this.chatRoom = chatRoom
+				this.prompt = reqeust.systemPrompt
+			}
+			ChatRoomDetail(chatRoom)
+		}
+	}
+	
+	suspend fun getChatRoom(chatRoomId: Int): ChatRoomDetail {
+		return query {
+			val chatRoom = findChatRoom(chatRoomId)
+			ChatRoomDetail(chatRoom)
+		}
+	}
+	
+	
+	suspend fun updateChatRoom(chatRoomId: Int, name: String) {
+		query {
+			val chatRoom = findChatRoom(chatRoomId)
+			chatRoom.name = name
+		}
+	}
+	
+	suspend fun deleteChatRoom(chatRoomId: Int) {
+		query {
+			val chatRoom = findChatRoom(chatRoomId)
+			chatRoom.delete()
+		}
+	}
+	
+	
+	suspend fun addChatHistory(chatRoomId: Int, message: String, role: Role): ChatHistory {
+		val chatHistory = query {
+			val chatRoom = findChatRoom(chatRoomId)
+			ChatHistoryEntity.new {
+				this.chatRoom = chatRoom
+				this.message = message
+				this.role = role
+			}
+		}
+		return ChatHistory(chatHistory)
+	}
+	
+	suspend fun listChatHitory(chatRoomId: Int): List<ChatHistory> {
+		return query {
+			val chatHistories = ChatHistoryEntity.find { ChatHistoryTable.chatRoomId eq chatRoomId }.toList()
+			chatHistories.map { ChatHistory(it) }
+		}
+	}
+	
+	suspend fun addSystemPrompt(chatRoomId: Int, prompt: String) {
+		query {
+			val chatRoom = findChatRoom(chatRoomId)
+			SystemPromptEntity.new {
+				this.chatRoom = chatRoom
+				this.prompt = prompt
+			}
+		}
+	}
+	
+	
+	suspend fun updateSystemPrompt(chatRoomId: Int, prompt: String) {
+		query {
+			val systemPrompt =
+				SystemPromptEntity.find { SystemPromptTable.chatRoomId eq chatRoomId }.firstOrNull() ?: throw NotFoundException(
+					"채팅방이 존재하지 않습니다."
+				)
+			systemPrompt.prompt = prompt
+		}
+	}
+	
+	
+	suspend fun deleteSystemPrompt(chatRoomId: Int) {
+		query {
+			val systemPrompt =
+				SystemPromptEntity.find { SystemPromptTable.chatRoomId eq chatRoomId }.firstOrNull() ?: throw NotFoundException(
+					"채팅방이 존재하지 않습니다."
+				)
+			systemPrompt.delete()
+		}
+	}
+	
+	suspend fun getAllMessages(chatRoomId: Int): List<BaseModel> {
+		return query {
+			val systemPrompt =
+				SystemPromptEntity.find { SystemPromptTable.chatRoomId eq chatRoomId }.map { SystemPrompt(it) }
+			
+			val chatRoom = findChatRoom(chatRoomId)
+			val chatHistories = ChatHistoryEntity.find { ChatHistoryTable.chatRoomId eq chatRoomId }.toList()
+			val messages = systemPrompt.map { SystemModel(it.prompt) } + chatHistories.map {
+				if (it.role == Role.user) UserModel(it.message)
+				else AssistantModel(it.message)
+			}
+			messages
+		}
+	}
+	
+	
+	suspend fun addUserMessage(chatRoomId: Int, message: String) {
+		return query {
+			val user = ChatRoomEntity.findById(chatRoomId)?.user ?: throw NotFoundException("채팅방이 존재하지 않습니다.")
+			if (user.chatCount >= 5) throw ReachedLimitException()
+			user.chatCount ++
+			addChatHistory(chatRoomId, message, Role.user)
+		}
+	}
+	
+	suspend fun addAssistantMessage(chatRoomId: Int, message: String) {
+		return query {
+			addChatHistory(chatRoomId, message, Role.assistant)
+		}
+	}
+	
+	
+	private fun findChatRoom(chatRoomId: Int): ChatRoomDetailEntity {
+		return ChatRoomDetailEntity.findById(chatRoomId) ?: throw NotFoundException("채팅방이 존재하지 않습니다.")
+	}
 }
